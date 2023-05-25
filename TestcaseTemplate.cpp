c6fe269c8add0988eb3b461df9cab76fc538cb2b
Jinx <95977042+Sathwikshaga@users.noreply.github.com>
2023-05-25 23:28:13 +0530

    Create TestcaseTemplate.cpp

---
+++ TestcaseTemplate.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void solution(){
+    
+    int n,k;
+    cin>>n>>k;
+    
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
+    for(int i=0;i<n;i++){
+      
+    }
+    cout<<endl;
+}
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    solution();
+	}
+	
+	return 0;
+}
