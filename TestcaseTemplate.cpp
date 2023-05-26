255ff6163564ab6cb0c86387f312dc67b2d0ce63
Jinx <95977042+Sathwikshaga@users.noreply.github.com>
2023-05-26 23:19:32 +0530

    Update TestcaseTemplate.cpp

--- TestcaseTemplate.cpp
+++ TestcaseTemplate.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
 using namespace std;
 
+#define int long long
+
+#define ll long long
+#define ld long double
+
+const ll MOD = 1e9 + 7;
+
 void solution(){
-    
     int n,k;
     cin>>n>>k;
     
     int a[n];
     for(int i=0;i<n;i++){
         cin>>a[i];
-    }
-    
+    }  
     for(int i=0;i<n;i++){
       
     }
     cout<<endl;
 }
 
-int main() {
+int32_t main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 	int t;
 	cin>>t;
 	
 	while(t--){
 	    solution();
 	}
-	
 	return 0;
 }
