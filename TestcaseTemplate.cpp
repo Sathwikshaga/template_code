25d0e206aece8d054b85fd2f98c5e912692b72d0
Jinx <95977042+Sathwikshaga@users.noreply.github.com>
2023-06-05 17:05:07 +0530

    Update TestcaseTemplate.cpp

--- TestcaseTemplate.cpp
+++ TestcaseTemplate.cpp
@@ -10,38 +10,40 @@ using namespace std;
 #define ll long long
 #define ld long double
 
-//Declaration
-
+// Declaration
 
 const ll mod = 1e9 + 7;
 
 //============================================//
 
-void solution(){
-	//Your code goes here.
-    int n,k;
-    cin>>n>>k;
-    
-//     vector<int> a(n);
-//     for(int i=0;i<n;i++){
-//         cin>>a[i];
-//     }
-//     for(int i=0;i<n;i++){
-    	
-//     }
-//     cout<< "YES" <<endl;
+void solution()
+{
+    // Your code goes here.
+    int n, k;
+    cin >> n >> k;
+
+    //     vector<int> a(n);
+    //     for(int i=0;i<n;i++){
+    //         cin>>a[i];
+    //     }
+    //     for(int i=0;i<n;i++){
+
+    //     }
+    //     cout<< "YES" <<endl;
 }
 
 //============================================//
 
-int32_t main(){
-	ios_base::sync_with_stdio(false);
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	int tt = 1;
-	cin>>tt;
-	
-	while(tt--){
-	    solution();
-	}
-	return 0;
+    int tt = 1;
+    cin >> tt;
+
+    while (tt--)
+    {
+        solution();
+    }
+    return 0;
 }
