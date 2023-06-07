1d55d40b25f69647a7788de8c49bdcb640120d36
Jinx <95977042+Sathwikshaga@users.noreply.github.com>
2023-06-07 16:57:55 +0530

    Update TestcaseTemplate.cpp

--- TestcaseTemplate.cpp
+++ TestcaseTemplate.cpp
@@ -22,14 +22,14 @@ void solution()
     int n, k;
     cin >> n >> k;
 
-    //     vector<int> a(n);
-    //     for(int i=0;i<n;i++){
-    //         cin>>a[i];
-    //     }
-    //     for(int i=0;i<n;i++){
-
-    //     }
-    //     cout<< "YES" <<endl;
+//         vector<int> a(n);
+//         for(int i=0;i<n;i++){
+//             cin>>a[i];
+//         }
+//         for(int i=0;i<n;i++){
+            
+//         }
+//         cout<< "YES" <<endl;
 }
 
 //============================================//
